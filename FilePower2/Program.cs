d58ce0797c62c1aed4717847da9d04e68f94d8a8
duducg <edu.sang.rocha@gmail.com>
2024-05-10 14:02:48 +0100

    Finished Exc 6

---
+++ FilePower2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FilePower2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //filename
+            string filename = args[0];
+            //bool flag
+            bool exit = false;
+            //Using StreamWriter
+            using StreamWriter sw = new StreamWriter(filename);
+                       
+            while (!exit)
+            {
+                
+                string userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    exit = true;                    
+                    break;
+                }
+                else
+                {
+                    sw.WriteLine(userInput);
+                }
+                
+            }
+            
+
+            
+            
+
+            
+            
+        }
+    }
+}
