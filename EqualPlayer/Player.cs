f05fd12b29703c3786a0b54740854b1a83cb27bb
duducg <edu.sang.rocha@gmail.com>
2024-05-10 13:24:24 +0100

    Finished Exercice 4

--- EqualPlayer/Player.cs
+++ EqualPlayer/Player.cs
@@ -8,12 +8,28 @@ namespace EqualPlayer
     public class Player
     {
         public PlayerClass PClass { get; }
-        public string Name { get; } 
+        public string Name { get; }
+        
         public Player (PlayerClass pClass, string name)
         {
             PClass = pClass;
             Name = name;
 
         }
+        
+        public override int GetHashCode()
+        {
+	        return PClass.GetHashCode() ^ Name.GetHashCode(); 
+        }
+
+        public override bool Equals(object obj)
+        {
+            Player other = obj as Player;
+        	if (other == null) return false;
+        	return PClass == other.PClass && Name == other.Name;
+        }
+        
+
+
     }
 }
--- EqualPlayer/Program.cs
+++ EqualPlayer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EqualPlayer
 {
@@ -6,7 +9,25 @@ namespace EqualPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Testing HashSet<Player>
+
+            //Create a HashSet
+            HashSet<Player> setOfPlayers = new HashSet<Player>()
+            {
+                new Player(PlayerClass.Tank, "Ana"),
+                new Player(PlayerClass.Slayer, "Paulo"),
+                new Player(PlayerClass.Tank, "Ana"),
+            };
+            
+            //testing a foreach:
+            foreach (Player p in setOfPlayers)
+            {
+                Console.WriteLine($"{p.Name} is a {p.PClass}");
+            }
+
+
         }
+
+       
     }
 }
