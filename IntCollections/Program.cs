83bcc9b395a62c0e8d02d0756e273027a57470dc
duducg <edu.sang.rocha@gmail.com>
2024-05-10 11:43:24 +0100

    Final commit Exercise 1

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -13,8 +13,40 @@ namespace IntCollections
             Queue<int> queue_a = new Queue<int>(ls_a);
             HashSet<int> hashSet_a = new HashSet<int> { 1, 10, -30, 10, -5 };
 
+            
             //All of these can use foreach since they all implement IEnumerable
-            //Print List values
+            //Printing using IEnumerable<T>
+            
+            List<IEnumerable<int>> collects = new List<IEnumerable<int>>() 
+            {
+                ls_a,
+                stack_a,
+                queue_a,
+                hashSet_a
+            };
+            
+            foreach(IEnumerable<int> cols in collects )
+            {
+                int count = 0;
+                string typeName = cols.GetType().Name;
+                typeName = typeName.Remove(typeName.Length-2);
+                Console.Write($"{typeName}:");
+                foreach(int i in cols)
+                {
+                    if (count > 0)Console.Write($", {i}");
+                    else Console.Write($" {i}");
+                    count++;
+
+                }
+                Console.Write("\n");
+                count = 0;
+            }
+
+    
+
+
+            #region Less efficient printing
+            /* //Print List values
             int count = 0;
             Console.Write("List:");            
             foreach (int i in ls_a)
@@ -56,7 +88,8 @@ namespace IntCollections
                 if (count > 0)Console.Write($", {i}");
                 else Console.Write($" {i}");
                 count++;                
-            }
+            } */
+            #endregion
             
             
            
