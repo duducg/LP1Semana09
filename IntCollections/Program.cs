5fa85badc63cbba8ad258a37e341e97c0d95646e
duducg <edu.sang.rocha@gmail.com>
2024-05-10 10:54:06 +0100

    List foreach done

---
+++ IntCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IntCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ls_a = new List<int>() { 1, 10, -30, 10, -5 };
+            //Stacks and Queues can take an ICollection as param
+            Stack<int> stack_a = new Stack<int>(ls_a); 
+            Queue<int> queue_a = new Queue<int>(ls_a);
+            HashSet<int> hashSet_a = new HashSet<int> { 1, 10, -30, 10, -5 };
+
+            //Print List values
+            int count = 0;
+            Console.Write("List:");            
+            foreach (int i in ls_a)
+            {
+                if (count > 0)Console.Write($", {i}");
+                else Console.Write($" {i}");
+                count++;
+            }
+            Console.Write("\n");
+            count = 0;
+            
+           
+        }
+    }
+}
