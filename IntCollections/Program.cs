7d685c887408439b8766e5970d4b7bfe50b42fa7
duducg <edu.sang.rocha@gmail.com>
2024-05-10 11:20:11 +0100

    Finished Exercice 1

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -13,6 +13,7 @@ namespace IntCollections
             Queue<int> queue_a = new Queue<int>(ls_a);
             HashSet<int> hashSet_a = new HashSet<int> { 1, 10, -30, 10, -5 };
 
+            //All of these can use foreach since they all implement IEnumerable
             //Print List values
             int count = 0;
             Console.Write("List:");            
@@ -23,7 +24,40 @@ namespace IntCollections
                 count++;
             }
             Console.Write("\n");
+            
+
+            //Print Stack Values
             count = 0;
+            Console.Write("Stack:");  
+            foreach (int i in stack_a )
+            {
+                if (count > 0)Console.Write($", {i}");
+                else Console.Write($" {i}");
+                count++;                
+            }
+            Console.Write("\n");
+            
+            //Print Queue Values
+            count = 0;
+            Console.Write("Queue:");  
+            foreach (int i in queue_a )
+            {
+                if (count > 0)Console.Write($", {i}");
+                else Console.Write($" {i}");
+                count++;                
+            }
+            Console.Write("\n");
+
+            //Print HashSet Values
+            count = 0;
+            Console.Write("HashSet:");  
+            foreach (int i in hashSet_a )
+            {
+                if (count > 0)Console.Write($", {i}");
+                else Console.Write($" {i}");
+                count++;                
+            }
+            
             
            
         }
